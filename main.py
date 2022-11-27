45705296869806b17c75dc943691aa3f7d674f70
democraz20 <mozartrasig@gmail.com>
2022-11-27 14:13:43 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import threading
+import time
+try: 
+    from pytube import Playlist, YouTube
+except ModuleNotFoundError: 
+    print('modules missing, installing them now.')
+    os.system("pip install pytube")
+
+def get_playlists(l: list[str]) -> list[str]:
+    urls: list[str] = []
+    for playlist in l:
+        playlist_urls = Playlist(playlist)
+        for link in playlist_urls: 
+            urls.append(link)
+    for link in playlist_urls: 
+        urls.append(link)
+    
+    return urls
+
+def download(url: str, destination: str, audio_only=False):
+    url = YouTube(url)
+    print(f"downloading {url.title}")
+    video = url.streams.filter(only_audio=audio_only).first()
+    out_file = video.download(output_path=destination)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+
+    print(f"downloaded {url.title} successfully")
+
+def start_cli():
+    urls: list[str] = []
+    while True:
+        i = str(input("  input playlist url >> "))
+        if i != "" :
+            urls.append(i)
+        else: break
+    # location = str(input(' input download location : '))
+    print("  Enter the destination (leave blank for current directory)")
+    destination = str(input("  >> ")) or '.'
+    for url in get_playlists(urls):
+        download(url, destination, iter=i)
+    print(urls) 
+
+if __name__ == '__main__':
+    # start_gui()
+    start_cli()
